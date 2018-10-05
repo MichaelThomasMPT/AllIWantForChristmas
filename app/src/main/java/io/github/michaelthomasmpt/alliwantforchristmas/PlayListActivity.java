c5db2b69894cdb1532f5b99cc4b62e94eb98fc30
MichaelThomasMPT <michaelthomas.mpt@gmail.com>
2018-10-05 23:25:18 +0800

    Adding in a RecycleView to display all of the plays recorded so far. Also adding in some pretty neat concept art! ;)

--- app/src/main/java/io/github/michaelthomasmpt/alliwantforchristmas/PlayListActivity.java
+++ app/src/main/java/io/github/michaelthomasmpt/alliwantforchristmas/PlayListActivity.java
@@ -1,13 +1,21 @@
 package io.github.michaelthomasmpt.alliwantforchristmas;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class PlayListActivity extends AppCompatActivity {
 
@@ -18,14 +26,30 @@ public class PlayListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //configure play list
+        List<PlayListItem> dummyItems = getPlayListItems();
+        RecyclerView playListView = (RecyclerView) findViewById(R.id.playListView);
+        PlayListItemAdapter adapter = new PlayListItemAdapter(dummyItems);
+        playListView.setAdapter(adapter);
+        playListView.setLayoutManager(new LinearLayoutManager(this));
+
+        //configure "new play" button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btnAddPlay);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                System.out.println("TODO...");
             }
         });
+    }
+
+    private List<PlayListItem> getPlayListItems() {
+        //FIXME don't mock this data
+        List<PlayListItem> dummyItems = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            dummyItems.add(new PlayListItem(new Date(), null));
+        }
+        return dummyItems;
     }
 
     @Override
---
+++ app/src/main/java/io/github/michaelthomasmpt/alliwantforchristmas/PlayListItem.java
@@ -0,0 +1,39 @@
+package io.github.michaelthomasmpt.alliwantforchristmas;
+
+import android.location.Location;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class PlayListItem {
+    private UUID id;
+    private Date timestamp;
+    private Location location;
+
+
+    public PlayListItem(Date timestamp, Location location) {
+        this.id = UUID.randomUUID();
+        this.timestamp = timestamp;
+        this.location = location;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
